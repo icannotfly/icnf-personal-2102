a5ec17c73932970a1c3835fde56cee39df7ff554
icannotfly <icannotfly@icannotfly.net>
2021-03-23 14:19:37 -0700

    remove image heights
    fixes #20

--- functions.php
+++ functions.php
@@ -191,4 +191,60 @@ function bootstrap_pagination( \WP_Query $wp_query = null, $echo = true ) {
 
 
 
+
+
+/*------------
+    images
+------------*/
+
+// removes width and height attributes from img tags - needed to get responsive images working
+// from https://wordpress.stackexchange.com/questions/29881/stop-wordpress-from-hardcoding-img-width-and-height-attributes
+/**
+ * This is a modification of image_downsize() function in wp-includes/media.php
+ * we will remove all the width and height references, therefore the img tag
+ * will not add width and height attributes to the image sent to the editor.
+ *
+ * @param bool false No height and width references.
+ * @param int $id Attachment ID for image.
+ * @param array|string $size Optional, default is 'medium'. Size of image, either array or string.
+ * @return bool|array False on failure, array on success.
+ */
+function stackexchange_image_downsize( $value = false, $id, $size ) {
+    if ( !wp_attachment_is_image($id) )
+        return false;
+
+    $img_url = wp_get_attachment_url($id);
+    $is_intermediate = false;
+    $img_url_basename = wp_basename($img_url);
+
+    // try for a new style intermediate size
+    if ( $intermediate = image_get_intermediate_size($id, $size) ) {
+        $img_url = str_replace($img_url_basename, $intermediate['file'], $img_url);
+        $is_intermediate = true;
+    }
+    elseif ( $size == 'thumbnail' ) {
+        // Fall back to the old thumbnail
+        if ( ($thumb_file = wp_get_attachment_thumb_file($id)) && $info = getimagesize($thumb_file) ) {
+            $img_url = str_replace($img_url_basename, wp_basename($thumb_file), $img_url);
+            $is_intermediate = true;
+        }
+    }
+
+    // We have the actual image size, but might need to further constrain it if content_width is narrower
+    if ( $img_url) {
+        return array( $img_url, 0, 0, $is_intermediate );
+    }
+    return false;
+}
+
+/* Remove the height and width refernces from the image_downsize function.
+ * We have added a new param, so the priority is 1, as always, and the new
+ * params are 3.
+ */
+add_filter( 'image_downsize', 'stackexchange_image_downsize', 1, 3 );
+
+
+
+
+
 ?>
